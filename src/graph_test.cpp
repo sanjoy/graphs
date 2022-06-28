86c4e7ef964551bf34aa41cc5e0ae347cbe4fc6c
Sanjoy Das <sanjoy@playingwithpointers.com>
2022-06-27 23:25:24 -0700

    Prefer static functions in test

--- src/graph_test.cpp
+++ src/graph_test.cpp
@@ -6,8 +6,7 @@
 
 using namespace graph;
 
-namespace {
-void TestIterators_0() {
+static void TestIterators_0() {
   std::vector<Graph::EdgeType> edges = {
       {0, 0}, {0, 2}, {0, 4}, {1, 3}, {3, 1},
   };
@@ -15,7 +14,7 @@ void TestIterators_0() {
   CHECK(!CheckConsistency(concrete_graph.get()).has_value());
 }
 
-void TestIterators_1() {
+static void TestIterators_1() {
   std::vector<Graph::EdgeType> edges = {
       {0, 1},
       {0, 2},
@@ -25,14 +24,13 @@ void TestIterators_1() {
   CHECK(!CheckConsistency(concrete_graph.get()).has_value());
 }
 
-void TestIterators_2() {
+static void TestIterators_2() {
   std::vector<Graph::EdgeType> edges = {
       {0, 1}, {0, 2}, {1, 2}, {0, 0}, {1, 1}, {2, 2},
   };
   std::unique_ptr<Graph> concrete_graph = CreateConcreteGraph(3, edges);
   CHECK(!CheckConsistency(concrete_graph.get()).has_value());
 }
-} // namespace
 
 #define TEST_LIST(F)                                                           \
   F(TestIterators_0)                                                           \
