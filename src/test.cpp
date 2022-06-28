089102999d524e110707952ee932c7073fb50452
Sanjoy Das <sanjoy@playingwithpointers.com>
2022-06-27 23:19:16 -0700

    Some test niceties:
     * --test-filter
     * --enable-logging-for-test
     * --quiet-mode
     * --enable-logging

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 namespace graph {
+namespace detail {
 static bool AnyAssertFailedFlag = false;
 
 void AssertFailed(const char *msg, const char *file, int line) {
@@ -12,4 +13,44 @@ void AssertFailed(const char *msg, const char *file, int line) {
 
 bool AnyAssertFailed() { return AnyAssertFailedFlag; }
 void ClearAssertFailed() { AnyAssertFailedFlag = false; }
+
+void ParseCmdOptions(int argc, char **argv, TestOptions *out) {
+#define CMD_ARG_STR_KEY_VALUE(sym)                                             \
+  do {                                                                         \
+    std::string sym_str = #sym;                                                \
+    for (char &c : sym_str)                                                    \
+      if (c == '_')                                                            \
+        c = '-';                                                               \
+    sym_str = std::string("--") + sym_str + "=";                               \
+    if (arg.find(sym_str) == 0) {                                              \
+      if (!out->sym.empty()) {                                                 \
+        std::cerr << "Only one " << sym_str << " allowed!" << std::endl;       \
+        abort();                                                               \
+      }                                                                        \
+      out->sym = arg.substr(sym_str.size());                                   \
+    }                                                                          \
+  } while (0)
+
+#define CMD_ARG_BOOL(sym)                                                      \
+  do {                                                                         \
+    std::string sym_str = #sym;                                                \
+    for (char &c : sym_str)                                                    \
+      if (c == '_')                                                            \
+        c = '-';                                                               \
+    sym_str = std::string("--") + sym_str;                                     \
+    if (arg == sym_str)                                                        \
+      out->sym = true;                                                         \
+  } while (0)
+
+  for (int i = 0; i < argc; i++) {
+    std::string arg = argv[i];
+
+    CMD_ARG_BOOL(quiet_mode);
+    CMD_ARG_BOOL(enable_logging);
+
+    CMD_ARG_STR_KEY_VALUE(enable_logging_for_test);
+    CMD_ARG_STR_KEY_VALUE(test_filter);
+  }
+}
+} // namespace detail
 } // namespace graph
--- src/test.hpp
+++ src/test.hpp
@@ -1,18 +1,31 @@
 #pragma once
 
+#include "logging.hpp"
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 namespace graph {
+namespace detail {
 void AssertFailed(const char *msg, const char *file, int line);
 bool AnyAssertFailed();
 void ClearAssertFailed();
+
+struct TestOptions {
+  bool quiet_mode = false;
+  bool enable_logging = false;
+  std::string enable_logging_for_test;
+  std::string test_filter;
+};
+
+void ParseCmdOptions(int argc, char **argv, TestOptions *out);
+} // namespace detail
 } // namespace graph
 
 #define CHECK(cond)                                                            \
   if (!(cond)) {                                                               \
-    AssertFailed(#cond, __FILE__, __LINE__);                                   \
+    ::graph::detail::AssertFailed(#cond, __FILE__, __LINE__);                  \
     return;                                                                    \
   }
 
@@ -38,7 +51,7 @@ void ClearAssertFailed();
     auto __a = (a);                                                            \
     auto __b = (b);                                                            \
     if (__a != __b) {                                                          \
-      AssertFailed(#a " != " #b, __FILE__, __LINE__);                          \
+      ::graph::detail::AssertFailed(#a " != " #b, __FILE__, __LINE__);         \
       std::cerr << #a << " = " << __a << std::endl;                            \
       std::cerr << #b << " = " << __b << std::endl;                            \
       return;                                                                  \
@@ -47,18 +60,26 @@ void ClearAssertFailed();
 
 #define __IMPL__RUN_TEST(fn)                                                   \
   do {                                                                         \
-    ::graph::ClearAssertFailed();                                              \
-    std::cout << "Running " << #fn << std::endl;                               \
-    fn();                                                                      \
-    if (::graph::AnyAssertFailed()) {                                          \
-      std::cout << "Failed " << #fn << std::endl;                              \
-      failed_tests.push_back(#fn);                                             \
-    } else                                                                     \
-      std::cout << "Passed " << #fn << std::endl;                              \
+    if (opts.test_filter.empty() || #fn == opts.test_filter) {                 \
+      if (#fn == opts.enable_logging_for_test || opts.enable_logging)          \
+        ::graph::EnableLogging();                                              \
+      ::graph::detail::ClearAssertFailed();                                    \
+      if (!opts.quiet_mode)                                                    \
+        std::cout << "Running " << #fn << std::endl;                           \
+      fn();                                                                    \
+      if (::graph::detail::AnyAssertFailed()) {                                \
+        std::cout << "Failed " << #fn << std::endl;                            \
+        failed_tests.push_back(#fn);                                           \
+      } else if (!opts.quiet_mode)                                             \
+        std::cout << "Passed " << #fn << std::endl;                            \
+      ::graph::DisableLogging();                                               \
+    }                                                                          \
   } while (0);
 
 #define RUN_MAIN(TEST_LIST)                                                    \
   do {                                                                         \
+    ::graph::detail::TestOptions opts;                                         \
+    ParseCmdOptions(argc, argv, &opts);                                        \
     std::cerr.precision(std::numeric_limits<double>::max_digits10);            \
     std::vector<std::string> failed_tests;                                     \
     TEST_LIST(__IMPL__RUN_TEST);                                               \
@@ -66,4 +87,4 @@ void ClearAssertFailed();
   } while (0)
 
 #define DEFINE_MAIN(TEST_LIST)                                                 \
-  int main() { RUN_MAIN(TEST_LIST); }
+  int main(int argc, char **argv) { RUN_MAIN(TEST_LIST); }
