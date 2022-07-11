fb56a1e32691250c2b875437035a79613d3255e4
Sanjoy Das <sanjoy@playingwithpointers.com>
2022-07-10 21:57:30 -0700

    Add one more test

--- src/counting_test.cpp
+++ src/counting_test.cpp
@@ -10,12 +10,17 @@ static void TestCountRegularGraphsWithDegree_4_2() {
 }
 
 static void TestCountRegularGraphsWithDegree_6_3() {
-  CHECK_EQ(CountRegularGraphsWithDegree(6, 3), 1);
+  CHECK_EQ(CountRegularGraphsWithDegree(6, 3), 2);
+}
+
+static void TestCountRegularGraphsWithDegree_6_2() {
+  CHECK_EQ(CountRegularGraphsWithDegree(6, 2), 2);
 }
 
 #define TEST_LIST(F)                                                           \
   F(TestCountRegularGraphsWithDegree_4_2)                                      \
   F(TestCountRegularGraphsWithDegree_6_3)                                      \
+  F(TestCountRegularGraphsWithDegree_6_2)                                      \
   (void)0;
 
 DEFINE_MAIN(TEST_LIST)
